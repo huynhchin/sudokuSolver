f6e234218f1cb7831b9709e240eba66ce3c22046
Chris Delaney <cadelaney3@gmail.com>
2018-04-23 21:43:27 -0700

    Add files via upload

--- sudoku.scala
+++ sudoku.scala
@@ -1,25 +1,66 @@
-//populate table
-def fillTable (arg: Array[Array[Int]]) {
-	for (i <- 0 until arr.length; j <- 0 until arr(0).length) {
-		println("Enter value:")
-		arr(i)(j) = scala.io.StdIn.readInt()
-	}
+class sudoku {
+  def print_board(board): Unit = {
+    for (x <- 0 to 9) {
+      for (y <- 0 to 9) {
+        print (board(x)(y))
+      }
+      println()
+    }
+  }
+
+  def empty_spot(board, spot): Unit = {
+    for (x <- 0 to 9) {
+      for (y <- 0 to 9) {
+        if (board(x)(y) == 0) {
+          spot(0) = x
+          spot(1) = y
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  def check_row(board, row, num): Unit = {
+    for (x <- 0 to 9) {
+      if (board(row)(x) == num) {
+        return true
+      }
+    }
+    return false
+  }
+
+  def check_col(board, col, num): Unit = {
+    for (y <- 0 to 9) {
+      if (board(y)(col) == num) {
+        return true
+      }
+    }
+    return false
+  }
+
+  def check_box(board, row, col, num): Unit = {
+    for (x <- 0 to 3) {
+      for (y <- 0 to 3) {
+        if (board(x+row)(y+col) == num) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  def is_valid_loc(board, row, col, num): Unit = {
+    if (!check_row(board, row, num) && !check_col(board, col, num)
+      && !check_box(board, row, col, num)) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  def solve(board): Unit = {
+
+  }
 }
-
-//print table
-def printTable (arg: Array[Array[Int]]) {
-	for {
-	  a <- 0 until row
-	  b <- 0 until col
-	} println(s"arr($a)($b) = ${arr(a)(b)}")
-}
-
-//create table
-println("Enter the number of rows:")
-val row = scala.io.StdIn.readInt()
-println("Enter the number of columns:")
-val col = scala.io.StdIn.readInt()
-val arr = Array.ofDim[Int](row,col)
-
-fillTable(arr)
-printTable(arr)
