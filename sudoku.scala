f673aab929bade213705d5c447ba1f7fb5e4d90f
huynhchin <chuynh@zagmail.gonzaga.edu>
2018-04-23 19:50:31 -0700

    created functions

--- sudoku.scala
+++ sudoku.scala
@@ -1,3 +1,19 @@
+//populate table
+def fillTable (arg: Array[Array[Int]]) {
+	for (i <- 0 until arr.length; j <- 0 until arr(0).length) {
+		println("Enter value:")
+		arr(i)(j) = scala.io.StdIn.readInt()
+	}
+}
+
+//print table
+def printTable (arg: Array[Array[Int]]) {
+	for {
+	  a <- 0 until row
+	  b <- 0 until col
+	} println(s"arr($a)($b) = ${arr(a)(b)}")
+}
+
 //create table
 println("Enter the number of rows:")
 val row = scala.io.StdIn.readInt()
@@ -5,14 +21,5 @@ println("Enter the number of columns:")
 val col = scala.io.StdIn.readInt()
 val arr = Array.ofDim[Int](row,col)
 
-//populate table
-for (i <- 0 until arr.length; j <- 0 until arr(0).length) {
-	println("Enter value:")
-	arr(i)(j) = scala.io.StdIn.readInt()
-}
-
-//print table
-for {
-  a <- 0 until row
-  b <- 0 until col
-} println(s"($a)($b) = ${arr(a)(b)}")
+fillTable(arr)
+printTable(arr)
