50978a9551dd76494e41b0aa24b67f8f02448648
sjoyce2 <35827474+sjoyce2@users.noreply.github.com>
2018-04-29 19:26:47 -0700

    Add files via upload

--- Sudoku.scala
+++ Sudoku.scala
@@ -1,32 +1,54 @@
+import scala.io.Source
+import scala.util.control.Breaks._
+
 object Sudoku extends App {
-  def readInput(i: Int, puzzle: Array[Array[Int]]) : Array[Array[Int]] = {
-    var line = scala.io.StdIn.readLine("Enter line number " + (i + 1) + " of the sudoku.");
-    while (line.length() != 9) {
-      println("That line is the wrong length!");
-      line = scala.io.StdIn.readLine("Enter line number " + (i + 1) + " of the sudoku.");
+  def readInput(puzzle: Array[Array[Int]]) : Array[Array[Int]] = {
+    //Reading sudoku board from file
+    scala.io.StdIn.readLine("Input board into 'board.txt' file. Press Enter when done.");
+    val inFile = Source.fromFile("board.txt");
+    var fileLine = 0;
+    var badLineLength = false;
+    for (line <- inFile.getLines()) {
+      // if line is the wrong length, say invalid input
+      // split line and convert it into numbers, storing them in an array
+
+
+      // old code
+      //    var line = scala.io.StdIn.readLine("Enter line number " + (i + 1) + " of the sudoku.");
+      //    while (line.length() != 9) {
+      //      println("That line is the wrong length!");
+      //      line = scala.io.StdIn.readLine("Enter line number " + (i + 1) + " of the sudoku.");
+      //    }
+
+      if (line.length() != 9) {
+        print("One of the lines is the wrong length. Starting over...");
+        badLineLength = true;
+        break;
+      }
+      var lineArr = line.split("");
+      try {
+        var numArr = new Array[Int](9);
+        for (j <- 0 to 8) {
+          var newNum = lineArr(j).toInt;
+          numArr(j) = newNum;
+        }
+        puzzle(fileLine) = numArr;
+        //      if (i < 8) {
+        //        readInput(i + 1, puzzle);
+        //      }
+        fileLine = fileLine + 1;
+      } catch {
+        case nfe: NumberFormatException => {
+          println("You didn't enter all numbers! Now we have to start over!");
+
+        }
+      }
     }
-    var lineArr = line.split("");
-    try {
-      var numArr = new Array[Int](9);
-      for (j <- 0 to 8) {
-        var newNum = lineArr(j).toInt;
-        numArr(j) = newNum;
-      }
-      puzzle(i) = numArr;
-      if (i < 8) {
-        readInput(i + 1, puzzle);
-      }
-      return puzzle;
-    } catch {
-      case nfe: NumberFormatException => {
-        println("You didn't enter all numbers! Now we have to start over!");
-        readInput(0, puzzle);
-      }
-    }
+    inFile.close();
+    return puzzle;
   }
 
-  //there is probably a faster way to check this. Something with another multidimensional array?
-  //not sure if that would actually be faster though...
+
   def checkIfValid(array: Array[Array[Int]]): Boolean = {
     var invalidVal = false;
     for (i <- 0 to 8) {
@@ -61,7 +83,7 @@ object Sudoku extends App {
     var puzzle = Array.ofDim[Int](9, 9);
     println("You will be entering the values of the unfinished sudoku puzzle, line by line. Enter" +
       " in a zero (0) character to\n signify an empty space.");
-    puzzle = readInput(0, puzzle);
+    puzzle = readInput(puzzle);
 
     if (checkIfValid(puzzle)) {
       println("That was an invalid puzzle, try again!");
